7c1b578027158c011e17a833ed7760f10b329aa9
Hai Thanh <timeltdco@gmail.com>
2018-12-08 16:18:19 +0700

    Initial commit

---
+++ app/src/main/java/layout/bai7/hatis/hatisbai7/DialogActivity.java
@@ -0,0 +1,14 @@
+package layout.bai7.hatis.hatisbai7;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class DialogActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
